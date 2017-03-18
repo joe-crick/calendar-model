54bb8caa5604f9ebe08e33aed2ddcbf847e3f9ea
Joe Crick <joe@josephcrick.com>
2017-03-18 11:16:50 +0100

    Add doc comments to name finder

--- src/name.finder.js
+++ src/name.finder.js
@@ -1,3 +1,11 @@
+
+/**
+ * @description Meta function for finding a name based on a number.
+ * 
+ * @export
+ * @param {any} nameSet 
+ * @returns 
+ */
 export default function numToNameFinder(nameSet) {
     return function getName(num) {
         return nameSet[num];
