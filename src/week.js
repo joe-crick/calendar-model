1099cec864a53d1e2431804cbc02cd1674f6c816
Joe Crick <joe@josephcrick.com>
2017-03-18 08:50:00 +0100

    Add Wednesday

--- src/week.js
+++ src/week.js
@@ -10,9 +10,10 @@ const WEEK_DAY_NAMES = {
     0: 'Sunday',
     1: 'Monday',
     2: 'Tuesday',
-    3: 'Thursday',
-    4: 'Friday',
-    5: 'Saturday'
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
 };
 
 /**
