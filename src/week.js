f91d7cb77b7a8087d5cdf448fde6ee04c5cd9b54
Joe Crick <joe@josephcrick.com>
2017-03-28 11:35:18 +0200

    Update comments. Build

--- src/week.js
+++ src/week.js
@@ -22,6 +22,19 @@ const WEEK_DAY_NAMES = {
  * @param {Function} formatDate A date formatting function. A default function is provided.
  * @param {number} numOfDays The number of days in a week. Zero-based, i.e., the default
  * number of days is 6.
+ * @example
+ * // Get a week using all the defaults
+ * const week = getWeek({startDate: '03/17/2016'});
+ *
+ * // Override a default (the date formatter)
+ * function americanFormat(date) {
+ *   const day = addLeadingZeroIfLessThanTen(date.getDate());
+ *   const month = addLeadingZeroIfLessThanTen(date.getMonth() + 1);
+ *   return `${month}/${day}/${date.getFullYear()}`;
+ * }
+ *
+ * const week = getWeek({startDate: '03/17/2017', formatDate: americanFormat})
+ *
  * @returns {Array<Day>} An array of Days.
  */
 export function getWeek({startDate, getEvents, formatDate, numOfDays = 6}) {
@@ -35,14 +48,22 @@ export function getWeek({startDate, getEvents, formatDate, numOfDays = 6}) {
  * @param {Function} getEvents An event binding function. A default function is provided.
  * @param {Function} formatDate A date formatting function. A default function is provided.
  * @param {number} numOfWeeks The number of weeks to return.
+ * @param {number} numOfDaysInWeek The number of days in the week
+ * @example
+ * // Get a week using all the defaults
+ * const week = getNWeeks({startDate: '03/17/2016'});
+ *
+ * // Override a default (the days in a week)
+ * const week = getWeek({startDate: '03/17/2017', numOfDaysInWeek: 5})
+ *
  * @returns {Array<Day>} A one-dimensional array of Days
  */
-export function getNWeeks({startDate, getEvents, formatDate, numOfWeeks}) {
+export function getNWeeks({startDate, getEvents, formatDate, numOfWeeks, numOfDaysInWeek = 6}) {
   if (!startDate) {
     return [];
   }
 
-  const week = getWeek({startDate, getEvents, formatDate});
+  const week = getWeek({startDate, getEvents, formatDate, numOfDays: numOfDaysInWeek});
 
   return !numOfWeeks
     ? week
@@ -56,6 +77,13 @@ export function getNWeeks({startDate, getEvents, formatDate, numOfWeeks}) {
  * @param {Function} getEvents An event binding function. A default function is provided.
  * @param {Function} formatDate A date formatting function. A default function is provided.
  * @param {number} numOfWeeks The number of weeks to return.
+ * @example
+ * // Get a week using all the defaults
+ * const week = getNWeeks({startDate: '03/17/2016'});
+ *
+ * // Override a default (the days in a week)
+ * const week = getWeek({startDate: '03/17/2017', numOfDaysInWeek: 5})
+ *
  * @returns {Array<Array<Day>>} A two-dimensional array of Days. Each sub-array contains a week's worth of days. Useful for tabular data representation.
  */
 export function getNWeeksNested({startDate, getEvents, formatDate, numOfWeeks}) {
@@ -115,14 +143,25 @@ export function getPrevWeekDay(date, daysInWeek = 7) {
  * @param {Object} weekDayNames Optional. An object with numeric indices 0 - n with values
  * corresponding to the weeks of the month. For example, the default value is:
  * {
-    0: 'Sunday',
-    1: 'Monday',
-    2: 'Tuesday',
-    3: 'Wednesday',
-    4: 'Thursday',
-    5: 'Friday',
-    6: 'Saturday'
-};
+ *   0: 'Sunday',
+ *   1: 'Monday',
+ *   2: 'Tuesday',
+ *   3: 'Wednesday',
+ *   4: 'Thursday',
+ *   5: 'Friday',
+ *   6: 'Saturday'
+ * };
+ * @example
+ *  const tagesnamen = {
+ *   0: 'Sontag',
+ *   1: 'Montag',
+ *   2: 'Dienstag',
+ *   3: 'Mittwoch',
+ *   4: 'Donnerstag',
+ *   5: 'Fritag',
+ *   6: 'Samstag'
+ * };
+ * const nameFinden = weekDayNameFinder(tagesnamen);
  * @returns {Function} A function that will find a week day name when given a number.
  */
 export function weekDayNameFinder(weekDayNames = WEEK_DAY_NAMES) {
